8cf9b6e53a942280560fee981a94b08ef4e33407
unknown <kaamal322@gmail.com>
2023-07-17 17:58:25 +0530

    first commit

---
+++ src/Unset_Bit.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Unset_Bit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        System.out.println(bitsetter(number,number2));
+    }
+
+    static int bitsetter(int number, int number2) {
+        int x=number;
+        for(int i=0;i<number2;i++){
+            x>>=1;
+        }
+        if((x&1)==1){
+            return number^(1<<number2);
+        }
+        else{
+            return number;
+        }
+
+
+    }
+}
